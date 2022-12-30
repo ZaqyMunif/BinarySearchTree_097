902700669cfd65a7d7c767b039cf5e51a7aacbc4
ZaqyMunif <zaqymunif17@gmail.com>
2022-12-30 17:15:55 +0700

    add class node

--- Activity8/Program.cs
+++ Activity8/Program.cs
@@ -6,6 +6,22 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree_097
 {
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+        //Constructor for the node class
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
+    }
+    /* A node class consist os three things, the information, refrrence to the right child, and refrence to the left child */
+
     internal class Program
     {
         static void Main(string[] args)
